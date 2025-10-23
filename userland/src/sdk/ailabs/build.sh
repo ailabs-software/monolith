3e404a7a5bbc807c3d73e479bbfb2c93e41160e7
Sheyenne York <support@shopwindow.io>
2025-10-23 18:45:30 -0400

    add cook

--- userland/src/sdk/ailabs/build.sh
+++ userland/src/sdk/ailabs/build.sh
@@ -7,3 +7,8 @@ mkdir /opt/monolith/userland/sdk/ailabs/bin
 
 cp /tmp/src/sdk/ailabs/fetch_repos.sh /opt/monolith/userland/sdk/ailabs/bin/fetch_repos.sh
 cp /tmp/src/sdk/ailabs/fetch_dart_pub.sh /opt/monolith/userland/sdk/ailabs/bin/fetch_dart_pub.sh
+
+# build cook
+cd /opt/ailabs/source/tools/cook/
+/opt/monolith/core/dart_sdk/bin/dart pub get
+/opt/monolith/core/dart_sdk/bin/dart compile exe main.dart -o /opt/monolith/userland/sdk/ailabs/bin/cook
